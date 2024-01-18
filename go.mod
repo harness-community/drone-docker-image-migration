f26480602e37e4261b9d4ddcb97e78396dbfa061
Akshit Agrawal <me@akshitagrawal.dev>
2024-01-18 22:17:14 +0530

    add access key, id support

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module drone/plugin/image-migration
 
 go 1.21.5
+
+require (
+	github.com/aws/aws-sdk-go v1.49.23 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
 func main() {
@@ -21,13 +28,38 @@ func main() {
 	imageName := os.Getenv("PLUGIN_IMAGE_NAME")
 	imageTag := os.Getenv("PLUGIN_IMAGE_TAG")
 
+	accessKeyId := os.Getenv("PLUGIN_AWS_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("PLUGIN_AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("PLUGIN_AWS_REGION")
+
 	// Authenticate and transfer image
-	authenticateAndTransferImage(sourceRegistry, sourceUsername, sourcePassword, sourceNamespace, destinationRegistry, destinationUsername, destinationPassword, destinationNamespace, imageName, imageTag)
+	authenticateAndTransferImage(sourceRegistry, sourceUsername, sourcePassword, sourceNamespace, destinationRegistry, destinationUsername, destinationPassword, destinationNamespace, imageName, imageTag, accessKeyId, secretAccessKey, region)
 }
 
-func authenticateAndTransferImage(sourceRegistry, sourceUsername, sourcePassword, sourceNamespace, destinationRegistry, destinationUsername, destinationPassword, destinationNamespace, imageName, imageTag string) {
+func authenticateAndTransferImage(sourceRegistry, sourceUsername, sourcePassword, sourceNamespace, destinationRegistry, destinationUsername, destinationPassword, destinationNamespace, imageName, imageTag, accessKeyId, secretAccessKey, region string) {
+
+	if sourceUsername == "AWS" && sourcePassword == "" {
+		// Get token from AWS
+		var err error
+		sourcePassword, err = getToken(accessKeyId, secretAccessKey, region)
+		if err != nil {
+			fmt.Println("Error getting token:", err)
+			os.Exit(1)
+		}
+	}
+
 	if sourceUsername != "" && sourcePassword != "" {
 		execCommand("skopeo", "login", "--username", sourceUsername, "--password", sourcePassword, sourceRegistry)
+	}
+
+	if destinationUsername == "AWS" && destinationPassword == "" {
+		// Get token from AWS
+		var err error
+		destinationPassword, err = getToken(accessKeyId, secretAccessKey, region)
+		if err != nil {
+			fmt.Println("Error getting token:", err)
+			os.Exit(1)
+		}
 	}
 
 	if destinationUsername != "" && destinationPassword != "" {
@@ -50,4 +82,45 @@ func execCommand(name string, arg ...string) {
 	}
 
 	fmt.Println(string(output))
+}
+
+func getToken(accessKeyId, secretAccessKey, region string) (string, error) {
+	// Initialize a session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+		Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+			AccessKeyID:     accessKeyId,
+			SecretAccessKey: secretAccessKey,
+		}),
+	})
+
+	if err != nil {
+		fmt.Println("Error creating session:", err)
+		return "", err
+	}
+
+	// Create an ECR client
+	svc := ecr.New(sess)
+
+	// Get the authorization token
+	input := &ecr.GetAuthorizationTokenInput{}
+	result, err := svc.GetAuthorizationToken(input)
+	if err != nil {
+		fmt.Println("Error getting authorization token:", err)
+		return "", err
+	}
+
+	var awsToken string
+
+	for _, data := range result.AuthorizationData {
+		token, err := base64.StdEncoding.DecodeString(*data.AuthorizationToken)
+		if err != nil {
+			fmt.Println("Error decoding token:", err)
+			return "", err
+		}
+
+		awsToken = string(token)
+	}
+
+	return strings.Split(awsToken, ":")[1], nil
 }
