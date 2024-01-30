1f5c583411a7b0b21208849e763b8bb810c1dca7
Akshit Agrawal <me@akshitagrawal.dev>
2024-01-30 16:48:50 +0530

    spec compatibility

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -7,151 +7,80 @@ package plugin
 import "testing"
 
 func TestValidateArgs(t *testing.T) {
-	type args struct {
-		Args Args
-	}
 	tests := []struct {
 		name    string
-		args    args
+		args    Args
 		wantErr bool
 	}{
 		{
 			name: "valid",
-			args: args{Args: Args{
-				SourceRegistry:       "registry.example.com",
-				SourceUsername:       "user",
-				SourcePassword:       "pass",
-				SourceNamespace:      "namespace",
-				ImageName:            "image",
-				ImageTag:             "tag",
-				DestinationRegistry:  "registry.example.com",
-				DestinationUsername:  "user",
-				DestinationPassword:  "pass",
-				DestinationNamespace: "namespace",
-			}},
+			args: Args{
+				Username:    "user",
+				Password:    "pass",
+				Source:      "source",
+				Destination: "destination",
+			},
 			wantErr: false,
 		},
 		{
-			name: "invalid source registry",
-			args: args{Args: Args{
-				SourceRegistry:      "",
-				SourceUsername:      "user",
-				SourcePassword:      "pass",
-				ImageName:           "image",
-				ImageTag:            "tag",
-				DestinationRegistry: "registry.example.com",
-				DestinationUsername: "user",
-				DestinationPassword: "pass",
-			}},
+			name: "invalid username",
+			args: Args{
+				Username:    "",
+				Password:    "pass",
+				Source:      "src",
+				Destination: "dest",
+			},
 			wantErr: true,
 		},
 		{
-			name: "invalid source username",
-			args: args{Args: Args{
-				SourceRegistry:      "registry.example.com",
-				SourceUsername:      "",
-				SourcePassword:      "pass",
-				ImageName:           "image",
-				ImageTag:            "tag",
-				DestinationRegistry: "registry.example.com",
-				DestinationUsername: "user",
-				DestinationPassword: "pass",
-			}},
+			name: "invalid password",
+			args: Args{
+				Username:    "user",
+				Password:    "",
+				Source:      "src",
+				Destination: "dest",
+			},
 			wantErr: true,
 		},
 		{
-			name: "invalid source password",
-			args: args{Args: Args{
-				SourceRegistry:      "registry.example.com",
-				SourceUsername:      "user",
-				SourcePassword:      "",
-				ImageName:           "image",
-				ImageTag:            "tag",
-				DestinationRegistry: "registry.example.com",
-				DestinationUsername: "user",
-				DestinationPassword: "pass",
-			}},
+			name: "invalid source",
+			args: Args{
+				Username:    "user",
+				Password:    "pass",
+				Source:      "",
+				Destination: "dest",
+			},
 			wantErr: true,
 		},
 		{
-			name: "invalid image name",
-			args: args{Args: Args{
-				SourceRegistry:      "registry.example.com",
-				SourceUsername:      "user",
-				SourcePassword:      "pass",
-				ImageName:           "",
-				ImageTag:            "tag",
-				DestinationRegistry: "registry.example.com",
-				DestinationUsername: "user",
-				DestinationPassword: "pass",
-			}},
+			name: "invalid destination",
+			args: Args{
+				Username:    "user",
+				Password:    "pass",
+				Source:      "src",
+				Destination: "",
+			},
 			wantErr: true,
+		},
+		{
+			name: "aws source",
+			args: Args{
+				Username:    "AWS",
+				Password:    "",
+				Source:      "src",
+				Destination: "dest",
+			},
+			wantErr: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			if err := ValidateArgs(tt.args.Args); (err != nil) != tt.wantErr {
-				t.Errorf("ValidateArgs() error = %v, wantErr %v", err, tt.wantErr)
+			err := validateArgs(&tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("validateArgs() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
-	}
-}
-
-func TestLoginToRegistry(t *testing.T) {
-	type args struct {
-		Username string
-		Password string
-		Registry string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name:    "invalid username",
-			args:    args{Username: "", Password: "pass", Registry: "registry.example.com"},
-			wantErr: true,
-		},
-		{
-			name:    "invalid password",
-			args:    args{Username: "user", Password: "", Registry: "registry.example.com"},
-			wantErr: true,
-		},
-		{
-			name:    "invalid registry",
-			args:    args{Username: "user", Password: "pass", Registry: ""},
-			wantErr: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-
-			if err := LoginToRegistry(tt.args.Username, tt.args.Password, tt.args.Registry); (err != nil) != tt.wantErr {
-				t.Errorf("LoginToRegistry() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestGetAWSPassword(t *testing.T) {
-	if _, err := GetAWSPassword(
-		"accessKeyID",
-		"secretAccessKey",
-		"region",
-	); err == nil {
-		t.Errorf("GetAWSPassword() error = %v", err)
-	}
-}
-
-func TestCopyImage(t *testing.T) {
-	if err := CopyImage(
-		"sourceImage",
-		"destinationImage",
-	); err == nil {
-		t.Errorf("CopyImage() error = %v", err)
 	}
 }
